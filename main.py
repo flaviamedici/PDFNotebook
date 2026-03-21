b4d2e40a991de9d6d86b54bba8786a43ee52e653
flaviamedici <flaviaomedici@gmail.com>
2026-03-21 12:35:03 -0700

    Initial pdf creator commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from fpdf import FPDF
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.add_page()
+
+pdf.set_font('Arial', 'B', size=12)
+pdf.cell(w=0, h=12, txt='Hello World!', align='L', ln=1)
+
+pdf.output("output.pdf")
